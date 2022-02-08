7a6759e4af5ab60e08c685d947bb5bbb0f0d453a
DLL <dmitry.lee@yandex.ru>
2022-02-08 23:04:01 +0300

    hw1 complete

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,26 +5,67 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        int size = size();
+        for (int i = 0; i < size; i++) {
+            storage[i] = null;
+        }
     }
 
     void save(Resume r) {
+        int index = 0;
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                index = i;
+                break;
+            }
+        }
+        storage[index] = r;
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < size(); i++) {
+            Resume resume = storage[i];
+            if (resume.uuid.equals(uuid)) {
+                return resume;
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        int size = size();
+        int index = 0;
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                index = i;
+                break;
+            }
+        }
+        for (int i = index; i < size; i++) {
+            storage[i] = storage[i + 1];
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        int size = size();
+        Resume[] resumes = new Resume[size];
+        for (int i = 0; i < size; i++) {
+            resumes[i] = storage[i];
+        }
+        return resumes;
     }
 
     int size() {
-        return 0;
+        int resumeCount = 0;
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                break;
+            }
+            resumeCount++;
+        }
+        return resumeCount;
     }
 }
