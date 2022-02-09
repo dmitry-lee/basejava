0998e12752b3ae85d0a569291ad874b6ab45f69f
DLL <dmitry.lee@yandex.ru>
2022-02-09 13:34:45 +0300

    fix and optimize

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,37 +3,30 @@
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int size;
 
     void clear() {
-        int size = size();
         for (int i = 0; i < size; i++) {
             storage[i] = null;
         }
+        size = 0;
     }
 
     void save(Resume r) {
-        int index = 0;
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] == null) {
-                index = i;
-                break;
-            }
-        }
-        storage[index] = r;
+        storage[size] = r;
+        size++;
     }
 
     Resume get(String uuid) {
-        for (int i = 0; i < size(); i++) {
-            Resume resume = storage[i];
-            if (resume.uuid.equals(uuid)) {
-                return resume;
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                return storage[i];
             }
         }
         return null;
     }
 
     void delete(String uuid) {
-        int size = size();
         int index = 0;
         for (int i = 0; i < size; i++) {
             if (storage[i].uuid.equals(uuid)) {
@@ -42,15 +35,17 @@ public class ArrayStorage {
             }
         }
         for (int i = index; i < size; i++) {
-            storage[i] = storage[i + 1];
+            if (i + 1 != size) {
+                storage[i] = storage[i + 1];
+            }
         }
+        size--;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        int size = size();
         Resume[] resumes = new Resume[size];
         for (int i = 0; i < size; i++) {
             resumes[i] = storage[i];
@@ -59,13 +54,6 @@ public class ArrayStorage {
     }
 
     int size() {
-        int resumeCount = 0;
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] == null) {
-                break;
-            }
-            resumeCount++;
-        }
-        return resumeCount;
+        return size;
     }
 }
